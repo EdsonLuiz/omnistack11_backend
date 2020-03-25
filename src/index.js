b4098e9fd43886a63ee4e762d814b983840370cd
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-25 15:32:39 -0300

    Configure database

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 
+const routes = require('./routes')
+
 const app = express()
 app.use(express.json())
+app.use(routes)
 
-app.get('/users', (req, res) => {
-  return res.json({ok: true})
-})
+
 
 app.listen(3333, () => {console.log('Servidor rodando')})
