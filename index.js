b9a2e57abad0abc6cb76afd637e9b499fdb303f4
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-25 15:02:08 -0300

    Add nodemon

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get('/users', (req, res) => {
   return res.json({ok: true})
 })
 
